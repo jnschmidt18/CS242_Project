506cf9cddbcbce1b2357c42bd644dc6bd80430bb
Josiah Schmidt <jnschmidt18@gmail.com>
2021-09-23 19:50:47 -0400

    constructors

--- src/ClackData.java
+++ src/ClackData.java
@@ -1,2 +1,20 @@
+import java.util.Date;
+
 public class ClackData {
+    String userName;
+    int type;
+    Date date;
+
+    public ClackData(String userName, int type){
+        this.userName = userName;
+        this.type = type;
+    }
+
+    public ClackData(int type){
+        this.userName = "Anon";
+        this.type = type;
+    }
+
+
 }
+
