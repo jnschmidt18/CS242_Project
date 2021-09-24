d0449fadb9153b6f2c5640284e9c8404c3ade0b5
Josiah Schmidt <jnschmidt18@gmail.com>
2021-09-23 20:02:58 -0400

    message

--- src/MessageClackData.java
+++ src/MessageClackData.java
@@ -1,3 +1,16 @@
-public class MessageClackData {
+public class MessageClackData extends ClackData{
 
+    String Message;
+
+    public MessageClackData(String userName, String Message, int type) {
+        super(userName, type);
+        this.Message = Message;
+    }
+
+
+
+    @Override
+    public String getData() {
+        return this.Message;
+    }
 }
