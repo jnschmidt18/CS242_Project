7334076cec650176bf062def18c630700241d79f
Josiah Schmidt <jnschmidt18@gmail.com>
2021-09-27 19:20:35 -0400

    message

--- src/test/testClackServer.java
+++ src/test/testClackServer.java
@@ -1,4 +1,13 @@
 package test;
 
 public class testClackServer {
+
+    public static void main(String[] args) {
+        int port=6000;
+        testClackServer test = new testClackServer();
+        testClackServer test2 = new testClackServer();
+
+        System.out.println(test.toString());
+        System.out.println(test==test2);
+    }
 }
