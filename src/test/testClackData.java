408c80b7fc0c509b7dcfcacdf560f6161611c1f2
Josiah Schmidt <jnschmidt18@gmail.com>
2021-09-27 19:26:06 -0400

    message

---
+++ src/test/testClackData.java
@@ -0,0 +1,11 @@
+package test;
+
+import data.*;
+
+public class testClackData {
+    public static void main(String[] args){
+        ClackData data = new MessageClackData("testUser", "test text", 1);
+
+
+    }
+}
